fd422e41ae5cb4fffeafff547ed7e9df549532a4
Neves <victorneveshs@gmail.com>
2023-06-29 16:44:00 -0300

    criacao do eureka server e do ms de clientes

---
+++ msclientes/src/main/java/com/msclientes/application/ClienteResource.java
@@ -0,0 +1,44 @@
+package com.msclientes.application;
+
+import com.msclientes.application.representation.ClienteSaveRequest;
+import com.msclientes.domain.Cliente;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+@Data
+@RequestMapping ("clientes")
+@RequiredArgsConstructor
+@RestController
+public class ClienteResource {
+    private final ClienteService service;
+
+    @GetMapping
+    public String status(){
+        return "ok";
+    }
+
+    @PostMapping
+    public ResponseEntity save(@RequestBody ClienteSaveRequest request) {
+        Cliente cliente = request.toModel();
+        service.save(cliente);
+        URI headerLocation = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .query("cpf={cpf}")
+                .buildAndExpand(cliente.getCpf())
+                .toUri();
+        return ResponseEntity.created(headerLocation).build();
+    }
+
+    @GetMapping(params = "cpf")
+    public ResponseEntity dadosCliente (@RequestParam("cpf")String cpf){
+        var cliente = service.getByCPF(cpf);
+        if(cliente.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(cliente);
+    }
+}
---
+++ msclientes/src/main/java/com/msclientes/application/ClienteService.java
@@ -0,0 +1,25 @@
+package com.msclientes.application;
+
+import com.msclientes.domain.Cliente;
+import com.msclientes.infra.repository.ClienteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ClienteService {
+
+    private final ClienteRepository repository;
+
+    @Transactional
+    public Cliente save(Cliente cliente){
+        return repository.save(cliente);
+    }
+
+    public Optional<Cliente> getByCPF(String cpf){
+        return repository.findByCpf(cpf);
+    }
+}
